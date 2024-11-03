3da0f6fa123cb0e91f5d54728dfa698dbd9a0b2e
Jared M Ashcraft <jaredmashcraft@gmail.com>
2024-11-03 14:30:50 -0500

    Hack added

---
+++ TADHack_Global_2024_Extra/TnidConsumerEscrow/src/TnidConsumerEscrowServer/TnidConsumerEscrowServer/Shared.cs
@@ -0,0 +1,7 @@
+﻿namespace TnidConsumerEscrowServer;
+
+public static class Shared
+{
+    public static string BuyerFullName { get; set; } = "";
+    public static string SellerFullName { get; set; } = "";
+}
---
+++ TADHack_Global_2024_Extra/TnidConsumerEscrow/src/TnidConsumerEscrowServer/TnidConsumerEscrowServer/TnidApi/TnidResponseParser.cs
@@ -0,0 +1,16 @@
+﻿namespace TnidConsumerEscrowServer.TnidApi;
+
+public static class TnidResponseParser
+{
+    public static string GetUserFullName(dynamic tnidPeopleSearchApiRequest)
+    {
+        var responseData = tnidPeopleSearchApiRequest.Data;
+
+        var firstUser = responseData.users[0];
+
+        var usersFirstName = (string)(firstUser.firstName);
+        var usersLastName = (string)(firstUser.lastName);
+        
+        return usersFirstName + " " + usersLastName; 
+    }
+}
